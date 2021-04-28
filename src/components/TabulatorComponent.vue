0be709fefe4c3318d718bc04f5e5cb7d518ca925
dustinleong-ipm <dustinleong@ipm.edu.mo>
2021-04-28 16:18:47 +0800

    v0

---
+++ src/components/TabulatorComponent.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Tabulator from 'tabulator-tables';
+
+export default defineComponent({
+  name: 'TabulatorComponent',
+  props: {
+    options: {
+      type: Tabulator.Options,
+      required: false,
+    },
+    modelValue: Object,
+    data: {
+      type: Object,
+      required: false,
+    },
+  },
+});
+</script>
